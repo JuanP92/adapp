567cba21be648da69213fe9266bf3b003f571845
Juan Pablo Ariza Garcia <jpabloariza93@gmail.com>
2022-06-20 21:12:45 -0500

    Arreglo a la tabla usuarios

---
+++ database/migrations/2022_06_21_020638_add_actualizacion_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('codigo', 10)->unique()->change();
+            $table->string('documento',50)->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('codigo', 10)->change();
+            $table->string('documento',50)->change();
+        });
+    }
+};
